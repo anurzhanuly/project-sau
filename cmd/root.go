541755d084ebf9cb469967b53cbe5109f3ccee88
nurzhanuly <nurzhanuly@kolesa.kz>
2022-05-15 00:14:08 +0600

    B-1 единая исходная точка

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,26 @@
 package cmd
 
-func Run() {
+import (
+	"anurzhanuly/project-sau/cmd/server"
+	"anurzhanuly/project-sau/environment"
+	"flag"
+)
 
+const defaultConfigPath = "config/development/sau.toml"
+
+func Run() {
+	configPath := flag.String(
+		"config",
+		defaultConfigPath,
+		"Путь до файла с конфигами проекта",
+	)
+	flag.Parse()
+
+	config := environment.NewConfig(*configPath)
+	err := config.Init()
+	if err != nil {
+		return
+	}
+
+	server.Init(config)
 }
