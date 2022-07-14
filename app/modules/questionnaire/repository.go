156fb7d9652b97e7e3421d2f8e05371853d44f6d
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-15 00:33:42 +0600

    B-3 добавляет скелеты-структуры для опросников

---
+++ app/modules/questionnaire/repository.go
@@ -0,0 +1,13 @@
+package questionnaire
+
+func GetQuestionnaireById(id int) Questionnaire {
+	var result Questionnaire
+
+	return result
+}
+
+func GetQuestionnaireByName(name string) Questionnaire {
+	var result Questionnaire
+
+	return result
+}
