ea8f3b1536aeec951cbc7fca6fba5041d6fe3df9
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-16 14:04:57 +0600

    B-3 переносит handler и ставит todo

--- app/http/handlers/handler.go
+++ app/http/handlers/handler.go
@@ -1,9 +1,11 @@
-package questionnaire
+package handlers
 
 import (
 	"anurzhanuly/project-sau/app/di"
 	"github.com/gin-gonic/gin"
 )
+
+// TODO: Надо бы вынести инициализацию сервиса модулей в middleware
 
 func ByName(c *gin.Context, di *di.DI) {
 
