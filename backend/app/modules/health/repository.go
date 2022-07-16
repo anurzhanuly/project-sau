7a0065c40ebae2658585bd3fdada792d4954670e
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-16 22:00:06 +0600

    B-3 feat
    запрос за всеми заболеваниями

--- backend/app/modules/health/repository.go
+++ backend/app/modules/health/repository.go
@@ -1,13 +1,37 @@
 package health
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
 
 type Repository struct {
 	collection *mongo.Collection
 }
 
-func (r Repository) getDiseases() []Disease {
-	var result []Disease
+func (r Repository) getDiseases() ([]Disease, error) {
+	var diseases []Disease
+	var err error
 
-	return result
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := r.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return diseases, err
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var disease Disease
+
+		err := cursor.Decode(&disease)
+		if err != nil {
+			return diseases, err
+		}
+
+		diseases = append(diseases, disease)
+	}
+
+	return diseases, err
 }
