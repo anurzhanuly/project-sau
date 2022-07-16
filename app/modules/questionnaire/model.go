b4f5702db48b91d7fbec0f5b8760071b713b1cfe
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-16 14:22:36 +0600

    B-3 опциональность IDшника

--- app/modules/questionnaire/model.go
+++ app/modules/questionnaire/model.go
@@ -3,7 +3,7 @@ package questionnaire
 import "encoding/json"
 
 type Questionnaire struct {
-	ID      int             `bson:"id" json:"id"`
+	ID      int             `bson:"id" json:"id,omitempty"`
 	Name    string          `bson:"name" json:"name"`
 	Content json.RawMessage `bson:"content" json:"content"`
 }
