ab13243ce9e7b9d597340086401aed8fd9b10bc4
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-19 01:39:00 +0600

    B-3 feat добавляет вспомогательную структуру для заболевания

--- app/modules/diseases/model.go
+++ app/modules/diseases/model.go
@@ -5,10 +5,17 @@ import (
 )
 
 type Disease struct {
-	ID              int                 `bson:"id" json:"id,omitempty"`
-	Name            string              `bson:"name" json:"name,omitempty"`
-	Recommendations string              `bson:"recommendations" json:"recommendations,omitempty"`
-	Conditions      map[string][]string `bson:"conditions" json:"conditions"`
+	ID              int                   `bson:"id" json:"id,omitempty"`
+	Name            string                `bson:"name" json:"name,omitempty"`
+	Recommendations string                `bson:"recommendations" json:"recommendations,omitempty"`
+	Conditions      map[string]Conditions `bson:"conditions" json:"conditions"`
+}
+
+type Conditions struct {
+	Value    string `bson:"value" json:"value"`
+	Type     string `bson:"type" json:"type"`
+	Compare  string `bson:"compare" json:"compare"`
+	Multiple bool   `bson:"multiple" json:"multiple"`
 }
 
 func (d Disease) meetsCriteria(answers *answers.Result) bool {
