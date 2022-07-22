3dbae20bfaed80be4bd0dfd690362226b59c0dcb
nurzhanuly <nurzhanuly@kolesa.kz>
2022-07-23 03:33:33 +0600

    B-3 feat наитупейшая логика проверки ответов, надо придумать что-то лучше

--- app/modules/diseases/model.go
+++ app/modules/diseases/model.go
@@ -3,6 +3,7 @@ package diseases
 import (
 	"anurzhanuly/project-sau/app/modules/answers"
 	"reflect"
+	"strconv"
 )
 
 type Disease struct {
@@ -29,13 +30,11 @@ type Recommendation struct {
 }
 
 func (d Disease) meetsCriteria(answers *answers.Result) bool {
-	for key, userAnswer := range answers.Answers {
-		conditionValue, exists := d.Conditions[key]
+	for key, condition := range d.Conditions {
+		answer, keyExists := answers.Answers[key]
 
-		if exists {
-			if compare(userAnswer, conditionValue) {
-				continue
-			}
+		if keyExists && isConditionMet(answer, condition) {
+			continue
 		}
 
 		return false
@@ -44,6 +43,41 @@ func (d Disease) meetsCriteria(answers *answers.Result) bool {
 	return true
 }
 
-func compare(answer []string, conditions Conditions) bool {
-	return reflect.DeepEqual(answer, conditions.Value)
+func isConditionMet(answer []string, conditions Conditions) bool {
+	if conditions.Multiple {
+		return reflect.DeepEqual(answer, conditions)
+	}
+
+	userValue := answer[0]
+	conditionValue := conditions.Value[0]
+
+	if conditions.Compare == "greater" {
+		userInput, err := strconv.Atoi(userValue)
+		if err != nil {
+			return false
+		}
+
+		condition, err := strconv.Atoi(conditionValue)
+		if err != nil {
+			return false
+		}
+
+		return userInput >= condition
+	}
+
+	if conditions.Compare == "less" {
+		userInput, err := strconv.Atoi(userValue)
+		if err != nil {
+			return false
+		}
+
+		condition, err := strconv.Atoi(conditionValue)
+		if err != nil {
+			return false
+		}
+
+		return userInput < condition
+	}
+
+	return userValue == conditionValue
 }
