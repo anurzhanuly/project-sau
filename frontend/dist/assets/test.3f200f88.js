8a94e16776cef077a38ffe349864a1e0e1b90ed2
Islam Karimov <ikz2018@bk.ru>
2022-07-26 23:26:34 +0600

    build: build
    frontend

--- frontend/dist/assets/test.3f200f88.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.65b6b36d.js";const s=e({id:"test",state:()=>({resultAnswers:{}})});export{s as u};
---
+++ frontend/dist/assets/test.69db44a1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.95388ec2.js";const s=e({id:"test",state:()=>({resultAnswers:{}})});export{s as u};
