43fa8800454e5758d91209ff86db96fb10a14de1
Nurzhanuly <isaxved@gmail.com>
2022-09-28 21:37:07 +0600

    B-4 компаратор рэнжа

--- app/modules/comparator/factory/products/single/range.go
+++ app/modules/comparator/factory/products/single/range.go
@@ -1,20 +1,38 @@
 package single
 
-import "anurzhanuly/project-sau/app/modules/data"
+import (
+	"anurzhanuly/project-sau/app/modules/data"
+	"strconv"
+)
 
 type Range struct {
 	UserAnswer       []string
 	DiseaseCondition data.Condition
 }
 
-func (n Range) DoesMatch() bool {
-	return false
+func (r Range) DoesMatch() bool {
+	compareValue, err := strconv.Atoi(r.DiseaseCondition.Value[0])
+	if err != nil {
+
+	}
+
+	compareValue2, err := strconv.Atoi(r.DiseaseCondition.Value[1])
+	if err != nil {
+
+	}
+
+	answer, err := strconv.Atoi(r.UserAnswer[0])
+	if err != nil {
+
+	}
+
+	return (compareValue <= answer) && (answer <= compareValue2)
 }
 
-func (n *Range) SetUserAnswer(answer []string) {
-	n.UserAnswer = answer
+func (r *Range) SetUserAnswer(answer []string) {
+	r.UserAnswer = answer
 }
 
-func (n *Range) SetCondition(conditions data.Condition) {
-	n.DiseaseCondition = conditions
+func (r *Range) SetCondition(conditions data.Condition) {
+	r.DiseaseCondition = conditions
 }
