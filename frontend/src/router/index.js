e0135c7dd1a879f23683009347493c0ff22a9be5
Islam Karimov <ikz2018@bk.ru>
2022-11-07 22:55:53 +0600

    fix:

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
