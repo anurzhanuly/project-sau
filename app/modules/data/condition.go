2af132bd01ee5799f082df045e139a302da87273
nurzhanuly <nurzhanuly@kolesa.kz>
2023-01-01 21:14:55 +0600

    Бэкенд: по стандарту

--- app/modules/data/condition.go
+++ app/modules/data/condition.go
@@ -15,7 +15,7 @@ type ConditionV1 struct {
 	Type           string   `bson:"type" json:"type"`
 	Compare        string   `bson:"compare" json:"compare"`
 	Multiple       bool     `bson:"multiple" json:"multiple"`
-	Question       string   `bson:"question" json:"question"`
+	Question       string   `bson:"questionName" json:"questionName"`
 	TestCase       string   `bson:"testCase,omitempty" json:"testCase,omitempty"`
 	IntValues      []int    `bson:"intValues,omitempty" json:"intValues,omitempty"`
 	ExcludedValues []string `bson:"excludedValues,omitempty" json:"excludedValues,omitempty"`
