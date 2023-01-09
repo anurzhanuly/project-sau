a63e81943f5d8f2e7f1bfcbe91235a401f0b0c04
nurzhanuly <nurzhanuly@kolesa.kz>
2023-01-10 00:23:08 +0600

    Бэкенд: выносит в /v1

--- app/http/backend/recommendations.go
+++ app/http/backend/recommendations.go
@@ -3,7 +3,6 @@ package backend
 import (
 	"anurzhanuly/project-sau/app/di"
 	"anurzhanuly/project-sau/app/modules/diseases"
-	v1 "anurzhanuly/project-sau/app/modules/diseases/v1"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -23,43 +22,6 @@ func GetAllRecommendations(c *gin.Context, di *di.DI) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": http.StatusOK,
 		"result": result,
-	})
-}
-
-// GetAllRecommendationsV1 возвращает все условия по заболеванию по новому формату
-func GetAllRecommendationsV1(c *gin.Context, di *di.DI) {
-	var result string
-	var err error
-	service := diseases.NewService(c, di)
-
-	result, err = service.FetchAllRecommendationsV1()
-	if err != nil {
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": result,
-	})
-}
-
-// AddDiseaseV1 добавляет условие по заболеванию, или апдейтит существующий по новому формату
-func AddDiseaseV1(c *gin.Context, di *di.DI) {
-	service := v1.NewService(c, di)
-
-	err := service.AddDisease()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status": http.StatusInternalServerError,
-			"result": fmt.Sprintf("произошла ошибка со стороны сервера: %s", err.Error()),
-		})
-
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": "success",
 	})
 }
 
