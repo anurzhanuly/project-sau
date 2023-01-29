4a8afee649982c7bae5e4b34f8d9a3845b0c369c
nurzhanuly <nurzhanuly@kolesa.kz>
2023-01-29 16:11:45 +0600

    Бэкенд: меняет поле для поиска

--- app/modules/diseases/v1/repository.go
+++ app/modules/diseases/v1/repository.go
@@ -76,7 +76,7 @@ func (r Repository) saveAll(diseases []Disease) error {
 	cxt, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
 	for _, disease := range diseases {
-		filter := bson.D{{"_id", disease.Name}}
+		filter := bson.D{{"_id", disease.ID}}
 
 		_, err := r.collection.ReplaceOne(cxt, filter, disease)
 		if err != nil {
