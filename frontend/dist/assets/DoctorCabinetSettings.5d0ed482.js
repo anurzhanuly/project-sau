9f236e74db802415d9aabfe8f229c82ece95d155
Almuker <73536819+Almuker@users.noreply.github.com>
2023-03-12 21:04:16 +0600

    finish autorization agreement

---
+++ frontend/dist/assets/DoctorCabinetSettings.5d0ed482.js
@@ -0,0 +1,1 @@
+import{a2 as e,a as t,b as c}from"./index.b1adbe29.js";const n={};function a(o,r){return t(),c("div",null,"wait")}const _=e(n,[["render",a]]);export{_ as default};
--- frontend/dist/assets/overlayeventbus.esm.979e3a4c.js
+++
@@ -1,1 +0,0 @@
-import{V as r}from"./index.cf8888dc.js";var e=r();export{e as O};
